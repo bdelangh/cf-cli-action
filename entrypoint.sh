0d7c4302f8763e5f8d2435efc2596da9dcbed444
Bart Delanghe <badelang@microsoft.com>
2020-11-22 17:02:49 +0100

    Switch to cf login

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,13 @@
 echo "Hello from Test"
 pwd
 cf --help
-cf api "$INPUT_CF_API"
-cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+#cf api "$INPUT_CF_API"
+#cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
-if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
-  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
-fi
+#if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
+#  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+#fi
+
+cf login -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD" -a "$INPUT_CF_API" -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 
 sh -c "cf $*"
