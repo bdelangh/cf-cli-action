34fdb9921d18d4194bc7058bd742359997dab426
Bart Delanghe <badelang@microsoft.com>
2020-11-22 17:25:10 +0100

    Added echo for variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 
 echo "Hello from Test"
 pwd
-cf --help
 #cf api "$INPUT_CF_API"
 #cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
 #if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
 #  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 #fi
-echo "Switching to user piper"
-su - piper
-echo "Swiched to user piper"
-pwd
+
+echo $HOME
+echo $USER_HOME
+echo $USER
+
 echo "Logging in to CF"
 cf login -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD" -a "$INPUT_CF_API" -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 echo "... Logged in to CF"
